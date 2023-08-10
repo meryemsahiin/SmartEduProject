6b8bb5514a31dd9ff99dee746a58993f9fcbf8cf
Meryem Sahin <sahinmeryem620@gmail.com>
2023-08-10 20:16:21 +0300

    lesson21

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,6 @@
+const nodemailer = require("nodemailer");
+
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
     res.status(200).render('index', {
@@ -22,3 +25,50 @@ exports.getLoginPage = (req, res) => {
         page_name: "login"
     });
   }
+
+exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+        page_name: "contact"
+    });
+  }
+
+exports.sendEmail =async (req, res) => {
+    const outputMessage = `
+    <h1>Mail Details</h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: "sahinmeryem620@gmail.com",
+        pass: "esjtxcdcfvvokefm",
+      }
+    });
+    
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+      // send mail with defined transport object
+      const info = await transporter.sendMail({
+        from: '"Smart EDU Contact From" <sahinmeryem620@gmail.com>', // sender address
+        to: "meerymsahin@gmail.com", // list of receivers
+        subject: "Smart EDU Contact From", // Subject line
+        html: outputMessage, // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      res.status(200).redirect('contact');
+
+      
+  }
+  main().catch(console.error);
+}
+
